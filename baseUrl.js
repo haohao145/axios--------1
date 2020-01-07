91cc457935cd7985f8e140c195a77db8a79fc3b4
haohao145 <13298181691@sina.cn>
2020-01-07 11:08:21 +0800

    接口封装方法1
    提交代码

---
+++ baseUrl.js
@@ -0,0 +1,18 @@
+//判断 当前环境  设置请求位置
+let baseUrl = '/proxyApi' // 本地代理
+
+import serverConfig from "../../public/serverConfig.json"
+
+switch (process.env.NODE_ENV) {
+    // case 'development':
+    //     baseUrl = 'http://10.0.111.24:9012/api' // 开发环境url
+    //     break
+    // case 'pre':
+    //     baseUrl = 'https://pre-server.feleti.cn' // 预上线环境url
+    //     break
+    case 'production':
+        baseUrl = serverConfig. // 生产环境url
+        break
+}
+
+export default baseUrl
